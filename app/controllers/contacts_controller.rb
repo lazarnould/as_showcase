d88e956625914ed662964ff1c724a8e09e2a945a
Laszlo Arnould <laszlo.arnould@gmail.com>
2017-05-11 12:52:35 +0200

    routing detail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = "Nous avons bien reçu votre demande d'informations, nous vous répondrons dans les plus brefs délais"
+      redirect_to contacts_new_path
     else
       flash[:danger] = "Une erreur est survenue, réessayez."
+      render :new
     end
   end
 
